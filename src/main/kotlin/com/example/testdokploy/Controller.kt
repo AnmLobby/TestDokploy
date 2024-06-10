ad6e0259665bfa3198fafc9c38b2401d839ebed5
Evan <ambroseyuwoquan@gmai.com>
2024-06-10 17:05:29 +0800

    1

--- src/main/kotlin/com/example/testdokploy/Controller.kt
+++ src/main/kotlin/com/example/testdokploy/Controller.kt
@@ -1,4 +1,15 @@
 package com.example.testdokploy
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
 class Controller {
+
+    @GetMapping("/greeting")
+    fun greeting() : String{
+        return "Hello, World!"
+    }
 }
