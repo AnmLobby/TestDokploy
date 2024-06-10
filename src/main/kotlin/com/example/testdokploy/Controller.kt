6c45700fc43fb8229f90470d1963a99a131eadec
Evan <ambroseyuwoquan@gmai.com>
2024-06-10 17:04:34 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/testdokploy/Controller.kt
@@ -0,0 +1,4 @@
+package com.example.testdokploy
+
+class Controller {
+}
---
+++ src/main/kotlin/com/example/testdokploy/TestDokployApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testdokploy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestDokployApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestDokployApplication>(*args)
+}
